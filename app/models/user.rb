4e280744e1e421dbdda84a9d161d9f03ee546852
jasonrhalpern <jasonrhalpern@gmail.com>
2015-06-02 12:30:39 -0400

    add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :lockable
 
+  validates :first_name, :last_name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: Devise::email_regexp }
+  validates :password, presence: true, confirmation: true, length: { in: 6..20 }, if: :password_required?
+  validate :password_complexity
+
   def password_complexity
     if password.present? and not password.match(/^(?=.*[\d[!@#$%\^*()_\-=?|;:.,<>]])(?=.*[a-zA-Z])[a-zA-Z0-9!@#$%\^*()_\-=?|;:.,<>]*$/)
       errors.add :password, 'Invalid password. Passwords must be 6-20 characters and contain at least one letter and one number or special character.'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,37 @@ describe User do
     expect(build_stubbed(:user)).to be_valid
   end
 
+  it 'is invalid without a first name' do
+    expect(build_stubbed(:user, first_name: nil)).to have(1).errors_on(:first_name)
+  end
+
+  it 'is invalid without a last name' do
+    expect(build_stubbed(:user, last_name: nil)).to have(1).errors_on(:last_name)
+  end
+
+  it 'is invalid without an email' do
+    expect(build_stubbed(:user, email: nil)).to have(2).errors_on(:email)
+  end
+
+  it 'is invalid without a valid email address' do
+    expect(build_stubbed(:user, email: 'test.com')).to have(1).errors_on(:email)
+  end
+
+  it 'is invalid without a unique email' do
+    create(:user, email: 'test@aol.com')
+    expect(build_stubbed(:user, email: 'test@aol.com')).to have(1).errors_on(:email)
+  end
+
+  it 'is invalid with a password that is too short' do
+    expect(build_stubbed(:user, password: 'pw4$')).to have(1).errors_on(:password)
+  end
+
+  it 'is invalid with a password that is too long' do
+    expect(build_stubbed(:user, password: 'w3#' * 7)).to have(1).errors_on(:password)
+  end
+
+  it 'is invalid with a password that doesn\'t have the right complexity' do
+    expect(build_stubbed(:user, password: 'password')).to have(1).errors_on(:password)
+  end
+
 end
