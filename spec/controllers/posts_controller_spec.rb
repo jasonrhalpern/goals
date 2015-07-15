22ff83e9068d305125095bf96ab873c7774c7cac
jasonrhalpern <jasonrhalpern@gmail.com>
2015-07-15 14:59:13 -0400

    couple of more post controller tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -20,4 +20,44 @@ describe PostsController do
     end
   end
 
+  describe "GET #new" do
+    it "assigns a new post to @post" do
+      get :new, :goal_id => johns_goal
+      expect(assigns(:post)).to be_a_new(Post)
+    end
+
+    it "renders the :new template" do
+      get :new, :goal_id => johns_goal
+      expect(response).to render_template("new")
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid attributes" do
+      it "saves the new post in the database" do
+        expect{
+          post :create, :goal_id => johns_goal, post: attributes_for(:post, :goal => johns_goal)
+        }.to change(Post, :count).by(1)
+      end
+
+      it "redirects to goals#index" do
+        post :create, :goal_id => johns_goal, post: attributes_for(:post, :goal => johns_goal)
+        expect(response).to redirect_to goal_posts_path(johns_goal)
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not save the new post in the database" do
+        expect{
+          post :create, :goal_id => johns_goal, post: attributes_for(:post, :goal => johns_goal, :content => nil)
+        }.to_not change(Post, :count)
+      end
+
+      it "re-renders the :new template" do
+        post :create, :goal_id => johns_goal, post: attributes_for(:post, :goal => johns_goal, :content => nil)
+        expect(response).to render_template("new")
+      end
+    end
+  end
+
 end
