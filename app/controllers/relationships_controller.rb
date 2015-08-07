3c7a22b54d2b6e388cff2aeaa238bbcb3aa25bee
jasonrhalpern <jasonrhalpern@gmail.com>
2015-08-07 17:49:18 -0400

    creating controller for relationships

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,43 @@
+class RelationshipsController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def followers_index
+    @followers = Relationship.includes(:users).where(followed_id: current_user.id).page(params[:page]).per(20)
+  end
+
+  def followings_index
+    @followings = Relationship.includes(:users).where(follower_id: current_user.id).page(params[:page]).per(20)
+  end
+
+  def create
+    @relationship = Relationship.new(followed_id: params[:user_id], follower_id: current_user.id)
+    authorize! :create, @relationship
+    respond_to do |format|
+      if @relationship.save
+        format.js { render :toggle }
+      else
+        format.js { render :toggle }
+      end
+    end
+  end
+
+  def destroy
+    @relationship = Relationship.find(params[:id])
+    authorize! :destroy, @relationship
+    respond_to do |format|
+      if @relationship.destroy
+        format.js { render :toggle }
+      else
+        format.js { render :toggle }
+      end
+    end
+  end
+
+  private
+
+  def relationship_params
+    params.require(:relationship).permit(:followed_id, :follower_id)
+  end
+
+end
